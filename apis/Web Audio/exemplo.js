b84547e31bee6b4e42a73fe6718f272b1e7c5827
gabriel <gabrielmattar41@gmail.com>
2016-04-21 12:21:44 -0300

    Volume funcionando, botao de pause colocado, estilo alterado

--- apis/Web Audio/exemplo.js
+++ apis/Web Audio/exemplo.js
@@ -1,27 +1,52 @@
 var AudioContext = window.AudioContext ||
                     window.webkitAudioContext;
-
 var context = new AudioContext();
-
 
 var musica1 = context.createBufferSource();
 var musica2 = context.createBufferSource();
-var gain = context.createGain();
+var volume =  context.createGain();
+
+var playing = false;
+
+document.getElementById("play").addEventListener("click", function(){
+  if(!playing){
+    musica1 = context.createBufferSource();
+    musica2 = context.createBufferSource();
+    musica1.connect(volume);
+    musica2.connect(volume);
+    volume.connect(context.destination);
 
 
-musica1.connect(gain);
-musica2.connect(gain);
-gain.connect(context.destination);
+    carregaSom('musica.mp3', musica1);
+    carregaSom('musica2.mp3', musica2);
+    musica1.start();
+
+    playing = true;
+  } else {
+    playing = false;
+    musica1.stop();
+  }
+});
 
 
-carregaSom('musica.mp3', musica1);
-carregaSom('musica2.mp3', musica2);
-
-musica1.start(context.currentTime);
 
 
+//Funcao de controle de volume
+volume.changeVolume = function(element) {
+  var fraction = parseInt(element.value) / parseInt(element.max);
+  //Uso de aumento de volume quadratico
+  console.log(fraction*fraction);
+  volume.gain.value = fraction * fraction;
+};
 
 
+
+
+
+
+
+
+//Funcao para carregar novos sons atraves de um caminho
 function carregaSom(url, musiquinha) {
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
