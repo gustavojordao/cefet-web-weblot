9950f93e9eb1913a2c2b26d33172f221e940b6af
gabriel <gabrielmattar41@gmail.com>
2016-04-21 14:24:24 -0300

    Adicionado interpolacao entre musicas

--- apis/Web Audio/exemplo.js
+++ apis/Web Audio/exemplo.js
@@ -4,31 +4,54 @@ var context = new AudioContext();
 
 var musica1 = context.createBufferSource();
 var musica2 = context.createBufferSource();
-var volume =  context.createGain();
+
+var filtromix1   = context.createGain();
+var filtromix2   = context.createGain();
+
+var volume  = context.createGain();
 
 var playing = false;
+
 
 document.getElementById("play").addEventListener("click", function(){
   if(!playing){
     musica1 = context.createBufferSource();
     musica2 = context.createBufferSource();
-    musica1.connect(volume);
-    musica2.connect(volume);
-    volume.connect(context.destination);
 
+    musica1.connect(filtromix1);
+    musica2.connect(filtromix2);
+
+    filtromix1.connect(volume);
+    filtromix2.connect(volume);
+
+    volume.connect(context.destination);
 
     carregaSom('musica.mp3', musica1);
     carregaSom('musica2.mp3', musica2);
     musica1.start();
-
+    musica2.start();
     playing = true;
   } else {
-    playing = false;
     musica1.stop();
+    musica2.stop();
+    playing = false;
   }
 });
 
 
+
+
+//Funcao para interpolar as 2 musicas
+crossfade = function(element) {
+  var x = parseInt(element.value) / parseInt(element.max);
+  // Use an equal-power crossfading curve:
+  var gain1 = Math.cos(x * 0.5*Math.PI);
+  var gain2 = Math.cos((1.0 - x) * 0.5*Math.PI);
+
+  console.log("Gain1: " + gain1 + "  Gain2: " + gain2);
+  filtromix1.gain.value = gain1;
+  filtromix2.gain.value = gain2;
+};
 
 
 //Funcao de controle de volume
@@ -38,8 +61,6 @@ volume.changeVolume = function(element) {
   console.log(fraction*fraction);
   volume.gain.value = fraction * fraction;
 };
-
-
 
 
 
